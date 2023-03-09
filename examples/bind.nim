6fff796ef21af0773a985b4f56dff5cc96ec3bc2
t-h-rk-turner <71654487+t-h-rk-turner@users.noreply.github.com>
2023-03-09 05:25:10 -0500

    add example using `bind`

---
+++ examples/bind.nim
@@ -0,0 +1,39 @@
+import std/json
+
+import ../webview
+
+var count: int
+
+const html = """
+<button id="increment">Tap me</button>
+<div>You tapped <span id="count">0</span> time(s).</div>
+<script>
+  const [incrementElement, countElement] =
+    document.querySelectorAll("#increment, #count");
+  document.addEventListener("DOMContentLoaded", () => {
+    incrementElement.addEventListener("click", () => {
+      window.increment().then(result => {
+        countElement.textContent = result.count;
+      });
+    });
+  });
+</script>
+"""
+
+proc main =
+  let w = create()
+
+  w.title = "Bind Example"
+  w.size = (480, 320)
+
+  w.bindCallback("increment") do (seq: string; req: string) -> JsonNode:
+    inc count
+
+    return %* {"count": count}
+
+  w.html = html
+
+  w.run()
+  w.destroy()  
+
+main()
