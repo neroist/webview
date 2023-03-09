6fb725f49b7e1d0a0713f80093d76b58c05162d0
Grave <pmpiga1@gmail.com>
2023-03-09 14:45:42 -0500

    add test

---
+++ tests/test_nim_api.nim
@@ -0,0 +1,24 @@
+import std/unittest
+
+import webview
+
+test "create a window, run app and terminate it.":
+  proc cbAssertArg(w: Webview, arg: pointer) {.cdecl.} = 
+    check w != nil
+    check cast[string](arg) == "arg"
+
+  proc cbTerminate(w: Webview, arg: pointer) {.cdecl.} = 
+    check arg == nil
+    w.terminate()
+
+  let w = newWebview(false)
+  
+  w.setSize(280, 320, WEBVIEW_HINT_NONE)
+  w.setTitle("Test")
+  
+  w.navigate("https://github.com/zserge/webview")
+  w.dispatch(cbAssertArg, cast[pointer]("arg"))
+  w.dispatch(cbTerminate, nil)
+
+  w.run()
+  w.destroy()
