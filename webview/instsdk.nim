b8c2520ee8e6760e85a6fcd6ef1480fafa01d191
Grave <pmpiga1@gmail.com>
2023-03-09 11:28:21 -0500

    auto delete compiled executable and add doc

--- webview/instsdk.nim
+++ webview/instsdk.nim
@@ -1,3 +1,5 @@
+## Script to install Webview2 SDK for Windows
+
 import std/os
 
 echo "\nInstalling Webview2 SDK..."
@@ -5,3 +7,5 @@ echo "\nInstalling Webview2 SDK..."
 createDir("libs/webview2")
 
 discard execShellCmd"curl -sSL https://www.nuget.org/api/v2/package/Microsoft.Web.WebView2 | tar -xf - -C libs/webview2"
+
+removeFile("webview/instsdk.exe")
